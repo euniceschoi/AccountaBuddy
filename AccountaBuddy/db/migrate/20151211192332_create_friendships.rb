866688a489ff272af0711bdcc4e396ee2eacea57
euniceschoi <euniceschoi93@gmail.com>
2015-12-11 11:28:07 -0800

    Add friendship table

--- AccountaBuddy/db/migrate/20151211192332_create_friendships.rb
+++ AccountaBuddy/db/migrate/20151211192332_create_friendships.rb
@@ -1,6 +1,11 @@
 class CreateFriendships < ActiveRecord::Migration
   def change
     create_table :friendships do |t|
+      t.references :user
+      t.integer :friend_id
+      t.references :category
+      t.boolean :accountabuddy
+      t.string :messages, array: true
 
       t.timestamps null: false
     end
