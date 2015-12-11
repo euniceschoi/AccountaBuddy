46d26d444bee57e6a2b10eb75671951628c6cb04
euniceschoi <euniceschoi93@gmail.com>
2015-12-11 14:26:52 -0800

    create all user views, added edit, index, new, and show .html.erb files

--- AccountaBuddy/config/routes.rb
+++ AccountaBuddy/config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  resources :users
   # You can have the root of your site routed with "root"
   root 'users#index'
-
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
