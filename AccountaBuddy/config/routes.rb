abc2569344f72b19eba21f990680983b351d00d5
Chris Mark & Eunice Choi <pair+chdmark.euniceschoi@devbootcamp.com>
2015-12-11 15:47:25 -0800

    Render facebook login page

--- AccountaBuddy/config/routes.rb
+++ AccountaBuddy/config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   resources :users
+  resources :sessions, only: [:create, :destroy]
+
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
   # You can have the root of your site routed with "root"
   root 'users#index'
   # Example of regular route:
