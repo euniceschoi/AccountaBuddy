f8c65d287d7c311d18b93c91223bc3169f916edd
TopGirlCoder <topgirlcoder@gmail.com>
2015-12-11 17:22:22 -0800

    seed file, categories controller, categories view, routes resources categories

---
+++ AccountaBuddy/app/controllers/categories_controller.rb
@@ -0,0 +1,17 @@
+class CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.all
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :user_id)
+  end
+
+end
--- AccountaBuddy/db/schema.rb
+++ AccountaBuddy/db/schema.rb
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 20151211195801) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
-    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
