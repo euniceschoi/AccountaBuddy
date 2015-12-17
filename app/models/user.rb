2dfda520e3abd9f057e2d33652c3658b2404c103
Chris Mark & Eunice Choi <pair+chdmark.euniceschoi@devbootcamp.com>
2015-12-16 21:45:53 -0800

    Add image table and rework facebook oauth create user logic to implement badges

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   geocoded_by :address
-  reverse_geocoded_by :latitude, :longitude, :address => :location
+  # reverse_geocoded_by :latitude, :longitude, :address => :location
   after_validation :reverse_geocode  # auto-fetch address
   # after_validation :geocode, :if => :address_changed?
   # validates_presence_of :name, :username, :email, :password, :about_me, :gender
@@ -34,10 +34,21 @@ class User < ActiveRecord::Base
       user.password = auth['uid']
       user.name = auth['info']['name']
       user.gender = auth['extra']['raw_info']['gender']
+      # user.picture_url = auth['info']['image']
 
-      user.save!
 
-      user
+      if User.exists?(user)
+        user
+      else 
+        user.save!
+          badge1 = Badge.new(name: "Fitness", description: "You're a fitness superstar!", user_id: user.id, badge_image_link: "fitness-badge.png")
+          badge2 = Badge.new(name: "Diet", description: "You're a Diet superstar!", user_id: user.id, badge_image_link:"diet-badge.png")
+          badge3 = Badge.new(name: "Hobbies", description: "You're a Hobbies superstar!", user_id: user.id, badge_image_link:"hobbies-badge.png")
+          badge4 = Badge.new(name: "Education", description: "You're a Education superstar!", user_id: user.id, badge_image_link:"education-badge.png")
+            p "YOU MADE THE REQUEST XHR"
+          badge1.save && badge2.save && badge3.save && badge4.save
+        user
+      end
 
   end
 
---
+++ db/migrate/20151217045646_add_column_to_users.rb
@@ -0,0 +1,5 @@
+class AddColumnToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :picture_url, :string
+  end
+end
