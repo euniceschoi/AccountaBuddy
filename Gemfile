77183686db07f112b2031b1944889855928c1b2f
Chris Mark & Eunice Choi <pair+chdmark.euniceschoi@devbootcamp.com>
2015-12-16 18:40:02 -0800

    recommendations form works, form shows on click of link and disappears after form is submitted

--- Gemfile
+++ Gemfile
@@ -31,6 +31,8 @@ gem 'omniauth-facebook'
 
 gem 'figaro'
 
+gem 'responders'
+
 
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,2 +1,35 @@
 class RecommendationsController < ApplicationController
+
+	def create
+		p "*" * 50 
+		p params 
+		p "*" * 50 
+		@recipient_id = params[:recommendation][:recipient_id].to_i
+		@recommendation_body = params[:recommendation][:body]
+		recommendation = Recommendation.new(user_id: current_user.id , recipient_id: @recipient_id, body: @recommendation_body)
+		
+
+		if recommendation.save
+			render "recommendations/_show_recommendations", locals: {recommendation: recommendation}, layout: false
+		end
+		# if recommendation.save
+		# 	respond_to do |format|
+		# 		format.js
+		# 	end
+		# 	# render json: {
+  #  #   		content: render_to_string({
+  #  #      partial: 'game_info',
+  #  #      layout: nil
+  #  #     })
+  #  #   }
+		# 	# 	render "recommendations/_show_recommendations", locals: {recommendation: recommendation}, layout: false
+		# end	
+	end
+
+	private
+
+	def recommendation_params
+		params.require(:recommendation).permit(:user_id, :recipient_id, :body)
+	end
+
 end
