8f621a91b9eb7e5b971f1cc281f34f31ed1c3f4a
euniceschoi <euniceschoi93@gmail.com>
2015-12-20 15:26:20 -0800

    more clean up + categories cont

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -1,7 +1,6 @@
 class BadgesController < ApplicationController
 
   def edit
-
   end
 
   def update
@@ -10,15 +9,6 @@ class BadgesController < ApplicationController
       badge.update(points: badge.points.to_i + 1 )
       badge.save
       redirect_to user_path(badge.user.id)
-
-
-      # respond_to do |format|
-      #   format.html 
-        
-      #   format.json{ render json: badge.points, status: 200 }
-      # p @badge
-      # p @badge
-      # redirect_to user_path(@badge.user.id)
   end
 
 end
