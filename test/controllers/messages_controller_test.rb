45c26af95358aeb7cb8ed21c1070ec6c3ab0049c
euniceschoi <euniceschoi93@gmail.com>
2016-01-01 22:57:19 -0800

    add message create route test

--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class MessagesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#create creates message' do
+    message_params = {
+      body: "Hello"
+      conversation_id: 1
+      user_id: 1
+    }
+
+    post :create, message: message_params
+
+    message = JSON.parse(response.body)
+
+    assert_equal message_params[:body], message["body"]
+  end
 end
