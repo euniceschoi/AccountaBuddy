6bfb24d54f9904b3c38971b22e5c83ac14c81b85
euniceschoi <euniceschoi93@gmail.com>
2017-02-12 16:23:23 -0500

    clean up categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,6 +5,7 @@ class CategoriesController < ApplicationController
 
   def index
     @categories = Category.all
+
     respond_to do |format|
       format.html
       format.json { render json: @geojson }
@@ -15,14 +16,10 @@ class CategoriesController < ApplicationController
     @user = User.find(params[:id])
     @category = Category.find(params[:id])
     @users_in_category = @category.users
-    if UserCategory.exists?(user_id: current_user.id, category_id: @category.id)
-      @user_category = true
-    end
-    if params[:search].present?
-      @users_close_by = @users_in_category.near(params[:search], 25)
-    else
-      @users_close_by = @users_in_category
-    end
+
+    check_if_user_subscribed
+    look_for_users_closeby
+
     respond_to do |format|
       format.html
       format.json { render json: @geojson }
@@ -30,17 +27,31 @@ class CategoriesController < ApplicationController
   end
 
   private
+ 
+  def category_params
+    params.require(:category).permit(:name)
+  end
 
   def load_user_categories
     @user_categories = UserCategory.all
   end
 
-  def get_mapbox_token
-    @mapbox_token = ENV['MAPBOX_TOKEN']
+  def check_if_user_subscribed
+    if UserCategory.exists?(user_id: current_user.id, category_id: @category.id)
+      @user_category = true
+    end
   end
 
-  def category_params
-    params.require(:category).permit(:name)
+  def look_for_users_closeby 
+    if params[:search].present?
+      @users_close_by = @users_in_category.near(params[:search], 25)
+    else
+      @users_close_by = @users_in_category
+    end
+  end
+
+  def get_mapbox_token
+    @mapbox_token = ENV['MAPBOX_TOKEN']
   end
 
 end
