fbd96abe46e0ed82ffb687eeddaf02e8889047c5
euniceschoi <euniceschoi93@gmail.com>
2017-02-13 16:41:06 -0500

    clean up accountabuddy request controller

--- app/controllers/accountabuddy_requests_controller.rb
+++ app/controllers/accountabuddy_requests_controller.rb
@@ -1,28 +1,34 @@
 class AccountabuddyRequestsController < ApplicationController
   def create
-    @recipient_id = params[:recipient][:recipient_id].to_i
-    @friendship_id = params[:recipient][:friendship_id].to_i
-    @accountabuddy_request = AccountabuddyRequest.new(user_id: current_user.id, recipient_id: @recipient_id, friendship_id: @friendship_id)
-    if @accountabuddy_request.save
-      redirect_to friendship_path(@friendship_id)
-    else
+    @accountabuddy_request = AccountabuddyRequest.new(
+      user_id: current_user.id, 
+      recipient_id: request_params[:recipient_id], 
+      friendship_id: request_params[:friendship_id]
+    )
+
+    if !@accountabuddy_request.save
       flash[:error] = "Accountabuddy request was unsuccesful. Please try again."
-      redirect_to friendship_path(@friendship_id)
     end
 
+    redirect_to friendship_path(request_params[:friendship_id])
   end
 
   def update
     @accountabuddy_request = AccountabuddyRequest.find(params[:id])
     @friendship_id = @accountabuddy_request.friendship_id
+    
     @friendship = Friendship.find(@friendship_id)
     @friendship.update(accountabuddy: true)
+
     if @friendship.save
       redirect_to friendship_path(@friendship_id)
     else
       redirect_to user_path(current_user.id)
     end
+  end
 
+  def request_params
+    params.require(:recipient).permit(:recipient_id, :friendship_id)
   end
 
 end
