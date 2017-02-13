b4d4c5c73238def41ddcb2ef0e0270b2533af8dd
euniceschoi <euniceschoi93@gmail.com>
2017-02-12 21:22:21 -0500

    clean up user categories controller

--- app/controllers/user_categories_controller.rb
+++ app/controllers/user_categories_controller.rb
@@ -1,13 +1,23 @@
 class UserCategoriesController < ApplicationController
 
   def create
-    @user_category = UserCategory.new(user_id: params[:category][:user_id], category_id: params[:category][:category_id])
+    @user_category = UserCategory.new(
+    	user_id: user_category_params[:user_id], 
+    	category_id: user_category_params[:category_id]
+  	)
+
     if @user_category.save
-      redirect_to category_path(params[:category][:category_id])
+      redirect_to category_path(user_category_params[:category_id])
     else
       flash[:error] = "Subscription failed. Please try again."
-      redirect_to category_path(params[:category][:category_id])
+      redirect_to category_path(user_category_params[:category_id])
     end
   end
+
+	private
+
+	def user_category_params
+		params.require(:category).permit(:user_id, :category_id)
+	end
 
 end
