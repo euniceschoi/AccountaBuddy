8848ab0e7a89d60f162625e031485d016111f506
euniceschoi <euniceschoi93@gmail.com>
2017-02-13 17:09:35 -0500

    clean up friend request controller more

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -20,9 +20,7 @@ class FriendRequestsController < ApplicationController
     @friend_request.update(friends: true)
 
     if @friend_request.save
-      @friendship = Friendship.create(user_id: current_user.id, friend_id: @friend_request.user_id, category_id: @category_id )
-      @inverse_friendship = Friendship.create(user_id: @friend_request.user_id, friend_id: current_user.id, category_id: @category_id)
-      @conversation = Conversation.create(sender_id:current_user.id, recipient_id: @friend_request.user_id )
+      create_friendship
     else
       flash[:error] = "Confirmation failed. Try again later!"
     end
@@ -35,4 +33,24 @@ class FriendRequestsController < ApplicationController
   def friend_request_params
     params.require(:recipientid).permit(:category_id, :recipient_id)
   end
+
+  def create_friendship
+    @friendship = Friendship.create(
+      user_id: current_user.id, 
+      friend_id: @friend_request.user_id, 
+      category_id: @category_id 
+    )
+
+    @inverse_friendship = Friendship.create(
+      user_id: @friend_request.user_id, 
+      friend_id: current_user.id, 
+      category_id: @category_id
+    )
+
+    @conversation = Conversation.create(
+      sender_id:current_user.id, 
+      recipient_id: @friend_request.user_id 
+    )    
+  end
+
 end
